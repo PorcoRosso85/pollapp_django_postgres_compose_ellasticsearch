9fef14521949dee02df013a0d9412f02bae9cd7f
root <root@DESKTOP-1PF4AT3.localdomain>
2022-11-11 06:45:20 +0900

    views returns HttpResponse, dont hardcode path

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
-    return render('', 'polls/index.html', {})
+    return render(request, 'polls/index.html', {})
+
+def detail(request, question_id):
+    return HttpResponse("Looking at question %s" % question_id)
+
+def results(request, question_id):
+    return HttpResponse("Looking at the result of question %s" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Voting on question %s" % question_id)
