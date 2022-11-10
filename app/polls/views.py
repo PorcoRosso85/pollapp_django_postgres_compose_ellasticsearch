f666756b7d7552553e95ff0130485785b1c7875e
root <root@DESKTOP-1PF4AT3.localdomain>
2022-11-11 07:06:36 +0900

    add models in views, not have to use HttpResponse

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-    return render(request, 'polls/index.html', {})
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
 
 def detail(request, question_id):
     return HttpResponse("Looking at question %s" % question_id)
