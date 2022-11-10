27693c0432896310d4786ddfa79b79d02588b52d
root <root@DESKTOP-1PF4AT3.localdomain>
2022-11-11 07:36:33 +0900

    shortcut allows us to try/except

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Question
@@ -8,7 +8,8 @@ def index(request):
     return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
 
 def detail(request, question_id):
-    return HttpResponse("Looking at question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     return HttpResponse("Looking at the result of question %s" % question_id)
