3727fa4a7f9b218e8b842a8e12acc38836d295ab
root <root@DESKTOP-1PF4AT3.localdomain>
2022-11-11 09:57:31 +0900

    only template, template shows us objects only

--- app/polls/views.py
+++ app/polls/views.py
@@ -27,4 +27,5 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse('polls:results', args=(question_id, )))
 
 def results(request, question_id):
-    return HttpResponse("Looking at the result of question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
